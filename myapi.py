2029c8997f51e26f005834ac57cfcd358352026b
Sharon <sasimanju1975@gmail.com>
2024-05-20 17:21:43 +0530

    changed

--- myapi.py
+++ myapi.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Path, HTTPException, status
 from typing import Optional
 
@@ -8,9 +9,12 @@ students = {
 }
 
 @app.get("/")
-def index():
+async def index():
     return {"message": "Welcome to the Student API!"}
 
 @app.get("/students/{student_id}")
 def get_student(student_id: Optional[int] = None):
     return students[student_id]
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
